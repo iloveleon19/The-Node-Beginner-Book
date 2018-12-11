9104088c55c12ae6ef48c71d02b9876b97727f96
iloveleon19 <iloveleon19@gmail.com>
2018-12-11 14:59:50 +0800

    處理檔案上傳

--- router.js
+++ router.js
@@ -1,12 +1,12 @@
-function route(handle, pathname, response) {
+function route(handle, pathname, response, request) {
     console.log("About to route a request for " + pathname);
 
     if (typeof(handle[pathname])==='function') {
-        handle[pathname](response);
+        handle[pathname](response, request);
     } else {
         console.log("No request handler found for " + pathname);
 
-        response.writeHead(404, {"content-Type": "text/plaon"});
+        response.writeHead(404, {"content-Type": "text/html"});
         response.write("404 Not found");
         response.end();
     }
